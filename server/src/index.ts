e6128d88c9bec8a92695f4bdfbbd4b5d25fd5ad0
VPagani <vr.paga@gmail.com>
2020-03-31 04:46:59 -0300

    Add server routes validation

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
 import * as Express from "express";
 import * as cors from "cors";
+import { errors } from "celebrate";
 import type { AddressInfo } from "net";
 
 import { routes } from "./routes";
@@ -9,6 +10,7 @@ const app = Express();
 app.use(cors());
 app.use(Express.json());
 app.use(routes);
+app.use(errors());
 
 const server = app.listen(process.env.PORT || 3333, () => {
     let { address, port } = server.address() as AddressInfo;
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { celebrate, Segments, Joi } from "celebrate";
 
 import * as SessionController from "./controllers/SessionController";
 import * as OngController from "./controllers/OngController";
@@ -7,13 +8,52 @@ import * as ProfileController from "./controllers/ProfileController";
 
 export const routes = Router();
 
-routes.post("/sessions", SessionController.create);
+routes.post("/sessions", celebrate({
+    body: Joi.object().keys({
+        id: Joi.string().required()
+    })
+}), SessionController.create);
 
 routes.get("/ongs", OngController.index);
-routes.post("/ongs", OngController.create);
 
-routes.get("/profile", ProfileController.index);
+routes.post("/ongs", celebrate({
+    body: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.string().required().regex(/^\d{10,11}$/),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2),
+    })
+}), OngController.create);
 
-routes.get("/incidents", IncidentController.index);
-routes.post("/incidents", IncidentController.create);
-routes.delete("/incidents/:id", IncidentController.remove);
+routes.get("/profile", celebrate({
+    headers: Joi.object({
+        authorization: Joi.string().required()
+    }).unknown()
+}), ProfileController.index);
+
+routes.get("/incidents", celebrate({
+    query: Joi.object().keys({
+        page: Joi.number()
+    })
+}), IncidentController.index);
+
+routes.post("/incidents", celebrate({
+    headers: Joi.object({
+        authorization: Joi.string().required()
+    }).unknown(),
+    body: Joi.object().keys({
+        title: Joi.string().required().min(5),
+        description: Joi.string().required(),
+        value: Joi.number().positive().required()
+    })
+}), IncidentController.create);
+
+routes.delete("/incidents/:id", celebrate({
+    headers: Joi.object({
+        authorization: Joi.string().required()
+    }).unknown(),
+    params: Joi.object().keys({
+        id: Joi.number().required()
+    })
+}), IncidentController.remove);
